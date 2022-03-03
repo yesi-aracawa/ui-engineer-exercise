340311b1b07e63d99325edd1d8ddf4287743bc83
SERNAVYY <yesica.sernavidale@maritz.com>
2022-03-02 19:33:06 -0600

    style: Add order and styling to the Additional Info cards

--- src/views/People/AdditionalInfo/AdditionalInfo.jsx
+++ src/views/People/AdditionalInfo/AdditionalInfo.jsx
@@ -1,13 +1,48 @@
 import React from 'react';
+import styled from "styled-components";
+import { getColor } from "../../../_starter/theme/theme";
+
+const MdAdditionalInfoCard = styled.div `
+  height: 226px;
+  background: ${getColor('bgGrey')};
+  border: 1px solid ${getColor('greyLightest')};
+  box-sizing: border-box;
+  margin: 0 0 10px 0;
+`;
+const LgAdditionalInfoCard = styled.div `
+  height: 428px;
+  background: ${getColor('bgGrey')};
+  border: 1px solid ${getColor('greyLightest')};
+  box-sizing: border-box;
+  margin: 0 0 10px 0;
+`;
+const SmAdditionalInfoCard = styled.div `
+  height: 215px;
+  background: ${getColor('bgGrey')};
+  border: 1px solid ${getColor('greyLightest')};
+  box-sizing: border-box;
+`;
+const XsmAdditionalInfoCard = styled.div `
+  height: 194px;
+  background: ${getColor('bgGrey')};
+  border: 1px solid ${getColor('greyLightest')};
+  box-sizing: border-box;
+  margin: 0 0 10px 0;
+`;
+const AditionalInfoContainer = styled.div`
+  width: 372px;
+  height: 100%;
+  margin: 0 10px 10px 10px;
+`;
 
 function AdditionalInfo() {
   return (
-    <div class='right-panel-container'>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
+    <AditionalInfoContainer>
+      <MdAdditionalInfoCard></MdAdditionalInfoCard>
+      <LgAdditionalInfoCard></LgAdditionalInfoCard>
+      <XsmAdditionalInfoCard></XsmAdditionalInfoCard>
+      <SmAdditionalInfoCard></SmAdditionalInfoCard>
+    </AditionalInfoContainer>
   );
 
 }
