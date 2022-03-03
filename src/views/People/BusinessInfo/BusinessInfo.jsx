a8ef716154b5b5a76d16c28d020f425d274e9fea
SERNAVYY <yesica.sernavidale@maritz.com>
2022-03-02 19:31:38 -0600

    style: Add styling to the  Business Cards Section
    * Add Social Media Icons
    * Add Styling displaying the Cards and Icons

--- src/views/People/BusinessInfo/BusinessInfo.jsx
+++ src/views/People/BusinessInfo/BusinessInfo.jsx
@@ -1,42 +1,59 @@
 import React from 'react';
 import styled from "styled-components";
 import { getColor } from "../../../_starter/theme/theme";
+import { default as CloudIcon } from "../../../_starter/shared/Icons/Cloud";
+import { default as LinkedInIcon } from "../../../_starter/shared/Icons/LinkedIn";
+import { default as TwitterIcon } from "../../../_starter/shared/Icons/Twitter";
+import { default as LinkIcon } from "../../../_starter/shared/Icons/Link";
+
 
 const BussinessCard = styled.div`
   height: 166px;
-  width: 300px;
-  left: 0px;
-  top: 0px;
-  border-radius: 0px;
-
-  position: absolute;
-  right: 0%;
-  bottom: 0%;
-
   background: ${getColor('white')};
+  border: 1px solid ${getColor('greyLightest')};
+  box-sizing: border-box;
+  border-radius: 0px;
+  margin: 0 0 10px 0;
+`;
+const MdBussinesCard =  styled.div `
+  height: 238px;
+  background: ${getColor('bgGrey')};
+  border: 1px solid ${getColor('greyLightest')};
+  box-sizing: border-box;
+  margin: 0 0 10px 0;
+`;
+const SmBussinesCard =  styled.div `
+  height: 144px;
+  background: ${getColor('bgGrey')};
+  border: 1px solid ${getColor('greyLightest')};
+  box-sizing: border-box;
+  margin: 0 0 10px 0;
+`;
+const LgBussinesCard =  styled.div `
+  height: 517px;
+  background: ${getColor('bgGrey')};
   border: 1px solid ${getColor('greyLightest')};
   box-sizing: border-box;
 `;
 const BussinessCardHeader = styled.div`
   height: 32px;
-  width: 300px;
-  left: 0px;
-  top: 0px;
   border-radius: 0px;
 `;
 const BusinessCardBody = styled.div`
   height: 134px;
-  width: 300px;
-  left: 0px;
-  top: 0px;
   border-radius: 0px;
+`;
+const SocialIcons = styled.div `
+  display: flex;
+  height: 24px;
+  .CloudIcon: {
+    background: ${getColor('blueLight')};
+  }
 `;
 const BusinessContainer = styled.div`
   height: 100%;
   width: 300px;
-  top: 120px;
-  left: 60px;
-  background-color: ${getColor('greyLightest')};
+  margin: 0 10px 10px 10px;
 `;
 
 function BusinessInfo() {
@@ -51,17 +68,17 @@ function BusinessInfo() {
           <h1>Gary Glover</h1>
           <h3>Marketing Manager</h3>
           <a href="#">Facebook</a>
-          <div class='social-icons'>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
+          <SocialIcons>
+            <CloudIcon></CloudIcon>
+            <LinkedInIcon></LinkedInIcon>
+            <TwitterIcon></TwitterIcon>
+            <LinkIcon></LinkIcon>
+          </SocialIcons>
         </BusinessCardBody>
       </BussinessCard>
-      <div></div>
-      <div></div>
-      <div></div>
+      <MdBussinesCard></MdBussinesCard>
+      <SmBussinesCard></SmBussinesCard>
+      <LgBussinesCard></LgBussinesCard>
     </BusinessContainer>
   );
 
